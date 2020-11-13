a8ffbaf30f47f5715d0d1cd973d61b686b3f49b3
Gurcan Hamali <gurcanhamali@gmail.com>
2020-11-13 11:03:22 +1100

    115. Navigating from a Child Component

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -5,8 +5,7 @@ import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
 
-const SearchScreen = props => {
-  console.log(props);
+const SearchScreen = ({ navigation }) => {
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
 
@@ -34,11 +33,17 @@ const SearchScreen = props => {
         <ResultsList
           results={filterResultsByPrice('$')}
           title='Cost Effective'
+          navigation={navigation}
         />
-        <ResultsList results={filterResultsByPrice('$$')} title='Bit Pricier' />
+        <ResultsList
+          results={filterResultsByPrice('$$')}
+          title='Bit Pricier'
+          navigation={navigation}
+        />
         <ResultsList
           results={filterResultsByPrice('$$$')}
           title='Big Spender'
+          navigation={navigation}
         />
       </ScrollView>
     </>
