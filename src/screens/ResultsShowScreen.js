e9049e8cf0a63762c7ac5b7f76e1ea5a72051c80
Gurcan Hamali <gurcanhamali@gmail.com>
2020-11-13 11:18:24 +1100

    118. Fetching a Single Restaurant

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({ navigation }) => {
-  const id = navigation.getParam('id');
+  const [result, setResult] = useState(null);
 
+  const id = navigation.getParam('id');
+  console.log(result);
+  const getResult = async id => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
   return (
     <View>
       <Text>Results Show</Text>
